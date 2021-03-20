34d467b69dd77eda217db8e9928f85e52817707d
Rick van de Loo <rickvandeloo@gmail.com>
2021-03-20 19:52:19 +0100

    add client script
    the program to check for changes in the terraform configuration and applying them

---
+++ homelabmanagerslave/homelabmanager.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [ -z "$HOMELABMANAGERHOST" ]; then
+    echo "You need to set HOMELABMANAGERHOST in your environment"
+else
+    exit 1
+fi
+
+export LIBVIRT_DEFAULT_URI="qemu:///system"
+
+# TODO: make nicer
+mkdir -p /tmp/terraform
+cd /tmp/terraform
+touch checksum
+chronic terraform init || /bin/true
+chronic wget "$HOMELABMANAGERHOST:4424/terraform/?host=$(hostname)" -O main.tf
+chronic terraform validate
+if ! cat checksum | md5sum --quiet -c; then
+    md5sum main.tf > checksum
+    terraform destroy --auto-approve
+    virsh list --all | grep -v debian10 | grep -v ubuntu18 | grep shut | awk '{print$2}' | xargs -I {} sh -c 'virsh destroy {} || /bin/true; virsh undefine {} || /bin/true' || /bin/true
+    cd /var/lib/libvirt/images
+    ls /var/lib/libvirt/images | grep -v bionic-server | grep -v debian-10 | xargs -I {} rm -rf "{}"
+    cd -
+    terraform apply --auto-approve
+fi
