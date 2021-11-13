4ffc840f66d8af00764d50858b2afe3abdc98ae5
Rick van de Loo <rickvandeloo@gmail.com>
2021-11-13 20:15:04 +0100

    only run tests for active salt states
    don't include server tests for states that have not been applied to the server

--- saltstack/salt/files/srv/machine-check/main.rkt
+++ saltstack/salt/files/srv/machine-check/main.rkt
@@ -1,4 +1,14 @@
 ; Tests for the homelabmanager configuration management
 ; Directory structure should correspond with the saltstack
 ; state file directories.
-(include "shellserver.rkt")
+
+(define applied-states (check-output '("/usr/bin/cat" "/srv/applied_states")))
+
+(define-syntax-rule (include-if-needed state path)
+  (if (member state applied-states)
+    (include path)
+    '()))
+
+(include-if-needed "core" "core.rkt")
+(include-if-needed "desktop" "desktop.rkt")
+(include-if-needed "shellserver" "shellserver.rkt")
