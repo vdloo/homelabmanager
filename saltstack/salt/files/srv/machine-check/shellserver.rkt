02598582900c56e30f4830d59e82d948e2071125
Rick van de Loo <rickvandeloo@gmail.com>
2021-12-04 11:36:08 +0100

    fix machine-check tests for archlinux

--- saltstack/salt/files/srv/machine-check/shellserver.rkt
+++ saltstack/salt/files/srv/machine-check/shellserver.rkt
@@ -1,16 +1,30 @@
 ; Install shellserver packages
-(check-packages-installed
- (list
-   "curl"
-   "git"
-   "jq"
-   "nmap"
-   "python3-mysqldb"
-   "python3-pip"
-   "python3-sqlparse"
-   "python3-venv"
-   "racket"
-   "screen"
-   "screenfetch"
-   "wget"
-   ))
+(let ((detected-os (detect-os))
+      (shellserver-packages (list
+			      "curl"
+			      "git"
+			      "jq"
+			      "nmap"
+			      "racket"
+			      "screen"
+			      "screenfetch"
+			      "wget"
+			      )))
+  (check-packages-installed
+    (if (equal? detected-os "arch")
+      ; Archlinux packages
+      (append shellserver-packages
+	      (list
+	        "python-mysqlclient"
+	        "python-pip"
+	        "python-sqlparse"
+	        "python-virtualenv"
+	      ))
+      ; Debian packages
+      (append shellserver-packages
+	      (list
+	        "python3-mysqldb"
+	        "python3-pip"
+	        "python3-sqlparse"
+	        "python3-venv"
+	      )))))
