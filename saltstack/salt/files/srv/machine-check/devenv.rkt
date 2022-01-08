044c01b42138792c838f9391bf55d61b5f400560
Rick van de Loo <rickvandeloo@gmail.com>
2022-01-08 19:24:56 +0100

    use check-file-contains
    see https://github.com/vdloo/machine-check/pull/11

--- saltstack/salt/files/srv/machine-check/devenv.rkt
+++ saltstack/salt/files/srv/machine-check/devenv.rkt
@@ -1,2 +1,8 @@
 (check-file-mode "/usr/local/bin/clone_development_projects.sh" 493)
 (check-file-mode "/home/{{ pillar['shellserver_unprivileged_user_name'] }}/code/projects/homelabmanager" 493)
+(check-file-contains
+  "/usr/local/bin/clone_development_projects.sh"
+  "vdloo/homelabmanager")
+(check-file-does-not-contain
+  "/usr/local/bin/configure_vim.sh"
+  "UNPRIVILEGED_USER=root")
