8b4a240ce2c0d6984c3e31f0cd9b8241ebeaadfe
Rick van de Loo <rickvandeloo@gmail.com>
2022-01-12 10:33:06 +0100

    make clean after copying machine-checks

--- saltstack/salt/files/usr/local/bin/install_machine_check_system_wide.sh
+++ saltstack/salt/files/usr/local/bin/install_machine_check_system_wide.sh
@@ -3,6 +3,7 @@ set -e
 cd /etc/machine-check
 raco pkg install --deps search-auto
 cp /srv/machine-check/* checks-to-perform/
+make clean
 cat << EOF > /sbin/machine-check
 #!/usr/bin/bash
 cd /etc/machine-check
