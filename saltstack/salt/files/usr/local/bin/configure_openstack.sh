3307e8f6d7ddda58552d9eeba3cc6afac06f9def
Rick van de Loo <rickvandeloo@gmail.com>
2022-08-21 14:34:42 +0200

    update openstack to stable/yoga
    updating from wallaby

--- saltstack/salt/files/usr/local/bin/configure_openstack.sh
+++ saltstack/salt/files/usr/local/bin/configure_openstack.sh
@@ -10,7 +10,7 @@ fi
 git clone https://github.com/openstack/devstack.git
 cd devstack
 git fetch
-git checkout stable/wallaby
+git checkout stable/yoga
 GIT_BASE_IF_NEEDED=""
 if [ -d /mnt/storage/openstack ]; then
     GIT_BASE_IF_NEEDED="GIT_BASE=file:///mnt/storage/openstack"
@@ -41,6 +41,20 @@ echo "Setting up security group rules"
 openstack security group rule create --proto icmp --dst-port 0 default
 openstack security group rule create --proto tcp --dst-port 1:65535 default
 
+echo "Waiting 5 seconds before proceeding to import Focal image"
+sleep 5
+echo "Importing Ubuntu Focal image"
+if [ -f /mnt/storage/openstack/focal-server-cloudimg-amd64.img ]; then
+    cp /mnt/storage/openstack/focal-server-cloudimg-amd64.img .
+else
+    wget -q https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+fi
+qemu-img resize focal-server-cloudimg-amd64.img +5G
+openstack image create --container-format bare --disk-format raw --file focal-server-cloudimg-amd64.img focal-server-cloudimg-amd64
+rm -f focal-server-cloudimg-amd64.img
+
+echo "Waiting 5 seconds before proceeding to import Buster image"
+sleep 5
 echo "Importing Debian Buster image"
 mkdir -p /opt/stack/downloaded_images
 cd /opt/stack/downloaded_images
@@ -52,16 +66,6 @@ fi
 qemu-img resize debian-10-openstack-amd64.raw +3G
 openstack image create --container-format bare --disk-format raw --file debian-10-openstack-amd64.raw debian-10-openstack-amd64
 rm -f debian-10-openstack-amd64.raw
-
-echo "Importing Ubuntu Focal image"
-if [ -f /mnt/storage/openstack/focal-server-cloudimg-amd64.img ]; then
-    cp /mnt/storage/openstack/focal-server-cloudimg-amd64.img .
-else
-    wget -q https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
-fi
-qemu-img resize focal-server-cloudimg-amd64.img +5G
-openstack image create --container-format bare --disk-format raw --file focal-server-cloudimg-amd64.img focal-server-cloudimg-amd64
-rm -f focal-server-cloudimg-amd64.img
 
 echo "Adding keypair"
 openstack keypair create --public-key /opt/stack/.ssh/id_rsa.pub homelabkey
