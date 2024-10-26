e90163b6d5a32d321949f8a959f52d8f50954d05
Rick van de Loo <rickvandeloo@gmail.com>
2024-10-26 21:02:33 +0200

    add apply_configurations script
    to apply kubernetes configurations

---
+++ kubernetes/apply_configurations.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+set -e
+SCRIPT_DIR="$(dirname -- "$BASH_SOURCE"; )";
+
+echo "Creating fresh temporary directory"
+rm -rf /tmp/kubernetes_configurations
+mkdir /tmp/kubernetes_configurations
+cp -R $SCRIPT_DIR/* /tmp/kubernetes_configurations
+cd /tmp/kubernetes_configurations
+
+# See https://prometheus-operator.dev/docs/getting-started/installation/
+echo "Installing kube-prometheus"
+git clone https://github.com/prometheus-operator/kube-prometheus.git
+cd kube-prometheus
+if ! kubectl get servicemonitors --all-namespaces | grep -q prometheus; then
+    kubectl create -f manifests/setup
+    until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+    kubectl create -f manifests/
+fi
