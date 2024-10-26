b8ef926293e5637341a076f29a2cd67d07bacfe1
Rick van de Loo <rickvandeloo@gmail.com>
2024-10-26 21:27:43 +0200

    install Frigate on Kubernetes

--- kubernetes/apply_configurations.sh
+++ kubernetes/apply_configurations.sh
@@ -14,6 +14,12 @@ git clone https://github.com/prometheus-operator/kube-prometheus.git
 cd kube-prometheus
 if ! kubectl get servicemonitors --all-namespaces | grep -q prometheus; then
     kubectl create -f manifests/setup
-    until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+    until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo; done
     kubectl create -f manifests/
 fi
+cd ..
+
+# See https://github.com/blakeblackshear/blakeshome-charts/tree/master/charts/frigate
+echo "Installing frigate"
+helm repo add blakeblackshear https://blakeblackshear.github.io/blakeshome-charts/
+helm upgrade --install my-release blakeblackshear/frigate -f frigate/values.yaml
