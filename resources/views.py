a284442e8377c64b0b86166aa0778734d4c662b3
Rick van de Loo <rickvandeloo@gmail.com>
2025-05-09 13:54:07 +0200

    update salt for bookworm

--- resources/views.py
+++ resources/views.py
@@ -119,18 +119,14 @@ runcmd:
             curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
             echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/latest jammy main" > /etc/apt/sources.list.d/salt.list
         else
-            curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/debian/10/amd64/latest/salt-archive-keyring.gpg
-            echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg] https://repo.saltproject.io/py3/debian/10/amd64/latest buster main" > /etc/apt/sources.list.d/salt.list
+            curl -fsSL https://packages.broadcom.com/artifactory/api/security/keypair/SaltProjectKey/public | tee /etc/apt/keyrings/salt-archive-keyring.pgp
+            curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/download/salt.sources | tee /etc/apt/sources.list.d/salt.sources
             if timeout 1 bash -c "</dev/tcp/192.168.1.252/80"; then
-                echo "deb [trusted=yes] http://192.168.1.252 buster main" > /tmp/temp_apt_sources.list
-                echo "deb [trusted=yes] http://192.168.1.252 buster-updates main" >> /tmp/temp_apt_sources.list
-                echo "deb [trusted=yes] http://192.168.1.252 buster-backports main" >> /tmp/temp_apt_sources.list
-                echo "deb [trusted=yes] http://192.168.1.252 buster-security main" >> /tmp/temp_apt_sources.list
+                echo "deb [trusted=yes] http://192.168.1.252 bookworm main" > /tmp/temp_apt_sources.list
+                echo "deb [trusted=yes] http://192.168.1.252 bookworm-updates main" >> /tmp/temp_apt_sources.list
             else
-                echo "deb http://deb.debian.org/debian/ buster main" > /tmp/temp_apt_sources.list
-                echo "deb http://deb.debian.org/debian/ buster-updates main" >> /tmp/temp_apt_sources.list
-                echo "deb http://archive.debian.org/debian/ buster-backports main" >> /tmp/temp_apt_sources.list
-                echo "deb http://security.debian.org/debian-security buster/updates main" >> /tmp/temp_apt_sources.list
+                echo "deb http://deb.debian.org/debian/ bookworm main" > /tmp/temp_apt_sources.list
+                echo "deb http://deb.debian.org/debian/ bookworm-updates main" >> /tmp/temp_apt_sources.list
             fi
             mv /tmp/temp_apt_sources.list /etc/apt/sources.list
         fi
